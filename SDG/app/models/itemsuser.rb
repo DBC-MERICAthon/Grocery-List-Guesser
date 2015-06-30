a6bb07fdaaa123ab3310699943570c873e9e69fc
Charles <whitfield.cw@gmail.com>
2015-06-30 14:32:19 -0700

    add models and migrations

---
+++ SDG/app/models/itemsuser.rb
@@ -0,0 +1,2 @@
+class Itemsuser < ActiveRecord::Base
+end
---
+++ SDG/db/migrate/20150630212443_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :auth_id
+      t.float :shopping_freq
+      t.datetime :recent_trip
+      t.integer :total_trips
+
+      t.timestamps null: false
+    end
+  end
+end
