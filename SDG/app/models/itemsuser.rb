7e6d4e5bd19224b5185ccbd87c64201933d36727
Charles <whitfield.cw@gmail.com>
2015-07-01 14:16:37 -0700

    change itemsuser lookup to utilize session helpers

--- SDG/app/models/itemsuser.rb
+++ SDG/app/models/itemsuser.rb
@@ -3,7 +3,7 @@ class Itemsuser < ActiveRecord::Base
   belongs_to :item
 
   def self.find_or_create(params)
-    target_relation = self.where(user_id: params[:user_id], item_id: params[:item_id]).first
-    target_relation ||= self.create(item_id: params[:item_id], user_id: params[:user_id])
+    target_relation = self.where(user_id: current_user, item_id: params[:item_id]).first
+    target_relation ||= self.create(item_id: params[:item_id], user_id: current_user)
   end
 end
