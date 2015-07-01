96d66ccea325a95f6d6ab3b7df014a88598f8cc8
Raghu Reddy <raghureddy.ram@gmail.com>
2015-07-01 12:08:22 -0700

    add private method, need to check ajax param names

--- SDG/app/controllers/items_controller.rb
+++ SDG/app/controllers/items_controller.rb
@@ -1,9 +1,15 @@
 class ItemsController < ApplicationController
 
   def create
-    @item = Item.find_or_create(params)
+    @item = Item.find_or_create(strong_params)
   end
 
-  #may need strong params
+  #Need to check strong params name here and assign after ajaxed in
+
+  private
+
+  def strong_params
+    #params.require(:some_name_of_json).permit(:name, :description, :category)
+  end
 
 end
