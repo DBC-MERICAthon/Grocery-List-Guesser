3f26be32795f24cb8ad4b51aa472c8d4200bbd69
Raghu Reddy <raghureddy.ram@gmail.com>
2015-07-01 15:11:00 -0700

    add ajax routes

--- SDG/app/assets/javascripts/application.js
+++ SDG/app/assets/javascripts/application.js
@@ -14,3 +14,32 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+// find_or_create the item
+
+// this should be the actual form for the item name
+
+var bindForm = function(e){
+  e.on('click', function(){
+    e.preventDefault();
+    addItem();
+  })
+}
+
+bindForm($('some-submit-css'));
+
+function addItem(el){
+  var item = $(el).serialize()
+  var category = args.category
+  var description =
+  var name = 
+  $.ajax({
+    url: "/items",
+    type: "POST",
+    data: {"name": $name, "description": $description, "category": $category}
+  })
+  .done(function(response){
+  })
+}
+
+
--- SDG/app/controllers/users_controller.rb
+++ SDG/app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
 
-  def index
-  end
+  # def index
+  # end
 
-  def new
-  end
+  # def new
+  # end
 
-  def create
-  end
+  # def create
+  # end
 
-  def show
-    # # pass @current_user
-    # @current_user = User.find(params[:id])
-    # p @current_user
-    # @current_items = Itemsuser.where(user_id:@current_user.id)
-    # p @current_items
-  end
+  # def show
+  #   # # pass @current_user
+  #   # @current_user = User.find(params[:id])
+  #   # p @current_user
+  #   # @current_items = Itemsuser.where(user_id:@current_user.id)
+  #   # p @current_items
+  # end
 
 end
