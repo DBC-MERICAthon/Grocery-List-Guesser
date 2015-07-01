7a18d0efa0f7a915ffb4a3a20e9c3b1773d2dd29
Raghu Reddy <raghureddy.ram@gmail.com>
2015-07-01 14:04:19 -0700

    Update to or equals for items itemsuser rb files

--- SDG/app/controllers/itemsusers_controller.rb
+++ SDG/app/controllers/itemsusers_controller.rb
@@ -2,7 +2,7 @@ class ItemsusersController < ApplicationController
 
 
   def create
-    
+   @itemsuser_relation = Itemsuser.find_or_create(params)
   end
 
   def update
@@ -12,6 +12,10 @@ class ItemsusersController < ApplicationController
   end
 
   # def show
+  # end
+
+  # def strong_params
+  #   params.require(:some_name_of_json).permit(:user_id, :item_id)
   # end
 
 end
