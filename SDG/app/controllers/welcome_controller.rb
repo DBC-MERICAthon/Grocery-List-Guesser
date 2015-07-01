516f5934315f5a9bc35c002efa7286c3d33c20e2
Raghu Reddy <raghureddy.ram@gmail.com>
2015-06-30 17:50:38 -0700

    edit login routes

--- SDG/app/controllers/welcome_controller.rb
+++ SDG/app/controllers/welcome_controller.rb
@@ -2,12 +2,14 @@ class WelcomeController < ApplicationController
   def index
   end
 
-  def create
+  def login
     if request.xhr? 
       # binding.pry
       u = User.where(auth_id: params["auth_id"]).first
       if u.nil?
         User.create( auth_id: params["auth_id"], name: params["name"] )
+      else
+        #get items data and render list homepage
       end
     end
   end
